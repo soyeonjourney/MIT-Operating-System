c90e0df9bd2750ae279c77e2a9a5663e87eecc4e
Tequila-Sunrise <steven.shao6174@gmail.com>
2022-11-21 02:33:48 +0800

    feat(lab4): [m] backtrace

--- kernel/printf.c
+++ kernel/printf.c
@@ -122,6 +122,7 @@ panic(char *s)
   printf("panic: ");
   printf(s);
   printf("\n");
+  backtrace();
   panicked = 1; // freeze uart output from other CPUs
   for(;;)
     ;
@@ -132,4 +133,21 @@ printfinit(void)
 {
   initlock(&pr.lock, "pr");
   pr.locking = 1;
+}
+
+void
+backtrace(void)
+{
+  uint64 ra;
+  uint64 fp, pgtop, pgbot;
+
+  fp = r_fp();
+  pgtop = PGROUNDUP(fp);
+  pgbot = PGROUNDDOWN(fp);
+  printf("backtrace: \n");
+  while(fp > pgbot && fp < pgtop) {
+    ra = *(uint64*)(fp - 8);
+    printf("%p\n", ra);
+    fp = *(uint64*)(fp - 16);
+  }
 }
